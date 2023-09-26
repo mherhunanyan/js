98cd882c858a41730189c1621a392d06640fece2
Mher Hunanyan <mher.hunanyan2@gmail.com>
2023-09-27 01:19:18 +0400

    improvements

--- homework-2/2file.js
+++ homework-2/2file.js
@@ -1,20 +1,26 @@
 //  Գրել ֆունկցիա, որը որպես արգումենտ ստանում է զանգված և վերադարձնում 
 // այդ զանգվածի մեծագույն և փոքրագույն տարրերի  ինդեքսների տարբերություն:
 
-function Indexdiffernce(arr) {
-    if (!arr.length) return null;
-
+function getMaxIndex(arr) {
     let maxIndex = 0;
-    let minIndex = 0;
-    for (let i = 0; i < arr.length; ++i) {
-        if (arr[i] > arr[maxIndex]) {
+    for (let i = 1; i < arr.length; ++i) {
+        if (arr[maxIndex] < arr[i]) {
             maxIndex = i;
         }
-        if (arr[i] < arr[minIndex]) {
+    }
+    return maxIndex;
+}
+function getMinIndex(arr) {
+    let minIndex = 0;
+    for (let i = 1; i < arr.length; ++i) {
+        if ( arr[minIndex] > arr[i]) {
             minIndex = i;
         }
     }
-    
-    return Math.abs(maxIndex - minIndex);
+    return minIndex;
 }
-console.log(Indexdiffernce([1,2,3,4,5]));
+
+function indexDiffernce(arr) {
+    return getMaxIndex(arr) - getMinIndex(arr);
+}
+console.log(indexDiffernce([1, -5, -10, 200, 0])); // 1
